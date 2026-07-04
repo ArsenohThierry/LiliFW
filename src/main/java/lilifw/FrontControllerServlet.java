5ebf10ee32c93a1935581414054bd2017b6cf4f1
RATANDRA Thierry Arsenoh <103485412+ArsenohThierry@users.noreply.github.com>
2026-07-04 08:43:31 +0300

    Re-Sprint 1

--- src/main/java/lilifw/FrontControllerServlet.java
+++ src/main/java/lilifw/FrontControllerServlet.java
@@ -1,6 +1,105 @@
 package lilifw;
 
-public class FrontControllerServlet {
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-    
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lilifw.utils.ControllerMethods;
+import lilifw.utils.Util;
+
+public class FrontControllerServlet extends HttpServlet {
+
+    // Map<String,List<Method>> listeMethodesAnnotes = new java.util.HashMap<>();
+    Map<String, ControllerMethods> urlToMethods = new HashMap<>();
+
+    public void init() throws ServletException {
+
+        // recuperer le package contenant dans web.xml
+        String packageLocation = this.getInitParameter("package");
+
+        // scanner tous les controlleurs et chacune de leurs methodes dans le package
+        // controller puis mapper avec les url
+        try {
+            Util.scanAllAnnotedControllers(packageLocation, urlToMethods);
+        } catch (Exception e) {
+            // afficher un errur
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+
+        String uri = request.getRequestURI();
+        String contextPath = request.getContextPath();
+        String url = uri.substring(contextPath.length());
+
+        if (url == null || url.equals("/")) {
+            request.setAttribute("annotatedMethods", getUrlToMethods());
+            request.getRequestDispatcher("/index.jsp").forward(request, response);
+            return;
+        }
+
+        request.setAttribute("url", url);
+
+        ControllerMethods methodeDeURL = urlToMethods.get(url);
+        if (methodeDeURL == null) {
+            String prefix = url.substring(0, url.lastIndexOf('/'));
+            if (prefix.isEmpty()) prefix = "/";
+
+            Map<String, ControllerMethods> matchingRoutes = new HashMap<>();
+            for (Map.Entry<String, ControllerMethods> entry : urlToMethods.entrySet()) {
+                if (entry.getKey().startsWith(prefix)) {
+                    matchingRoutes.put(entry.getKey(), entry.getValue());
+                }
+            }
+
+            request.setAttribute("notFoundUrl", url);
+            request.setAttribute("matchingRoutes", matchingRoutes);
+            request.getRequestDispatcher("/route.jsp").forward(request, response);
+            return;
+        }
+
+        request.setAttribute("controllerName", methodeDeURL.getControllerName());
+        request.setAttribute("methodName", methodeDeURL.getMethode().getName());
+        request.getRequestDispatcher("/route.jsp").forward(request, response);
+    }
+
+    public Map<String, ControllerMethods> getUrlToMethods() {
+        return urlToMethods;
+    }
+
+    // public Map<String, List<Method>> getListeMethodesAnnotes() {
+    // return listeMethodesAnnotes;
+    // }
+
 }
---
+++ src/main/java/lilifw/utils/Util.java
@@ -0,0 +1,55 @@
+package lilifw.utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import lilifw.annotation.Controller;
+import lilifw.annotation.UrlMapping;
+
+public class Util {
+    // public static void scanAllAnnotedControllers(String
+    // packageLocation,List<Class<?>> liste_controller) throws Exception{
+    public static void scanAllAnnotedControllers(String packageLocation, Map<String, ControllerMethods> urlToMethods)
+            throws Exception {
+
+        packageLocation = packageLocation.replace(".", "/") + "/controllers";
+        URL url = Thread.currentThread().getContextClassLoader().getResource(packageLocation);
+
+        if (url == null) {
+            System.out.println("No controllers package found at: " + packageLocation);
+            return;
+        }
+
+        File dossier = new File(url.toURI());
+
+        for (File file : dossier.listFiles()) {
+            // recuperer tous les noms de classes
+            if (file.isFile() && file.getName().endsWith(".class")) {
+                // transformer en Nom de classes sans .class
+                String className = packageLocation.replace("/", ".") + "." + file.getName().replace(".class", "");
+                // transformer en Class et verifier si annote controller , si oui ajouter dans
+                // la liste si non continue
+                Class<?> clazz = Class.forName(className);
+
+                if (clazz.isAnnotationPresent(Controller.class)) {
+                    for (Method method : clazz.getDeclaredMethods()) {
+                        if (method.isAnnotationPresent(UrlMapping.class)) {
+                            String urlMethod = method.getAnnotation(UrlMapping.class).value();
+                            urlToMethods.putIfAbsent(urlMethod, new ControllerMethods(clazz.getName(), method));
+                        }
+                    }
+                }
+                else {
+                    continue;
+                }
+
+            }
+        }
+    }
+}
